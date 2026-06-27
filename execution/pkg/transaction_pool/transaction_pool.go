f87a8c1ae1e07bd4e9640bf492206b4d49833443
Tên của bạn <email@example.com>
2026-06-27 07:08:36 +0000

    perf(execution): refactor TransactionPool to sharded lock-free architecture for E2E TPS ingestion scaling

--- execution/pkg/transaction_pool/transaction_pool.go
+++ execution/pkg/transaction_pool/transaction_pool.go
@@ -3,6 +3,7 @@ package transaction_pool
 import (
 	"fmt"
 	"sort"
+	"sync"
 	"sync/atomic"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -10,59 +11,41 @@ import (
 	"github.com/meta-node-blockchain/meta-node/types"
 )
 
-type addTxReq struct {
-	tx    types.Transaction
-	reply chan error
+const NumShards = 256
+
+type txPoolKey struct {
+	addr  common.Address
+	nonce uint64
 }
 
-type getTxsReq struct {
-	reply chan []types.Transaction
+type poolShard struct {
+	mu              sync.RWMutex
+	transactions    []types.Transaction
+	transactionKeys map[txPoolKey]bool
+	txHashMap       map[common.Hash]types.Transaction
 }
 
-type countReq struct {
-	reply chan int
-}
-
-type evictReq struct {
-	count int
-	reply chan int
-}
-
-type getTxReq struct {
-	hash  common.Hash
-	reply chan getTxResp
-}
-
-type getTxResp struct {
-	tx types.Transaction
-	ok bool
+func newPoolShard() *poolShard {
+	return &poolShard{
+		transactions:    make([]types.Transaction, 0),
+		transactionKeys: make(map[txPoolKey]bool),
+		txHashMap:       make(map[common.Hash]types.Transaction),
+	}
 }
 
 type TransactionPool struct {
-	// Channels for Actor Pattern (lock-free)
-	addTxCh       chan addTxReq
-	addTxsCh      chan []types.Transaction
-	getTxsCh      chan getTxsReq
-	countCh       chan countReq
-	evictCh       chan evictReq
-	getTxByHashCh chan getTxReq
-
+	shards     [NumShards]*poolShard
 	NotifyChan chan struct{} // GO-2: Event channel to notify workers of new transactions
 	count      int64         // Atomic counter for lock-free count queries
 }
 
 func NewTransactionPool() *TransactionPool {
 	tp := &TransactionPool{
-		addTxCh:       make(chan addTxReq),
-		addTxsCh:      make(chan []types.Transaction),
-		getTxsCh:      make(chan getTxsReq),
-		countCh:       make(chan countReq),
-		evictCh:       make(chan evictReq),
-		getTxByHashCh: make(chan getTxReq),
-		NotifyChan:    make(chan struct{}, 1),
+		NotifyChan: make(chan struct{}, 1),
 	}
-
-	go tp.loop()
+	for i := 0; i < NumShards; i++ {
+		tp.shards[i] = newPoolShard()
+	}
 	return tp
 }
 
@@ -74,181 +57,206 @@ func (tp *TransactionPool) notifyWork() {
 	}
 }
 
-type txPoolKey struct {
-	addr  common.Address
-	nonce uint64
-}
-
-// loop runs in a background goroutine and processes all requests sequentially,
-// eliminating the need for any sync.Mutex locks and preventing contention.
-func (tp *TransactionPool) loop() {
-	transactions := make([]types.Transaction, 0)
-	transactionKeys := make(map[txPoolKey]bool)
-	txHashMap := make(map[common.Hash]types.Transaction)
-
-	for {
-		select {
-		case req := <-tp.addTxCh:
-			key := txPoolKey{addr: req.tx.FromAddress(), nonce: req.tx.GetNonce()}
-			if transactionKeys[key] {
-				logger.Info("Transaction already exists in pool, skipping key addr=%s nonce=%d", key.addr.Hex(), key.nonce)
-				req.reply <- fmt.Errorf("transaction already exists in pool, skipping")
-				continue
-			}
-
-			// CROSS-CHAIN DEBUG logic (unchanged)
-			if req.tx.ToAddress() == common.HexToAddress("0x00000000000000000000000000000000B429C0B2") {
-				logger.Info("📥 [POOL-ARRIVE] Cross-chain TX entered mempool: hash=%s from=%s nonce=%d pool_size=%d",
-					req.tx.Hash().Hex()[:16], req.tx.FromAddress().Hex()[:10], req.tx.GetNonce(), len(transactions)+1)
-			}
-
-			transactions = append(transactions, req.tx)
-			transactionKeys[key] = true
-
-			h := req.tx.Hash()
-			if h != (common.Hash{}) {
-				txHashMap[h] = req.tx
-			}
-
-			atomic.StoreInt64(&tp.count, int64(len(transactions)))
-			tp.notifyWork()
-			req.reply <- nil
-
-		case txs := <-tp.addTxsCh:
-			addedAny := false
-			for _, tx := range txs {
-				key := txPoolKey{addr: tx.FromAddress(), nonce: tx.GetNonce()}
-				if !transactionKeys[key] {
-					transactions = append(transactions, tx)
-					transactionKeys[key] = true
-
-					h := tx.Hash()
-					if h != (common.Hash{}) {
-						txHashMap[h] = tx
-					}
-					addedAny = true
-				}
-			}
-			if addedAny {
-				atomic.StoreInt64(&tp.count, int64(len(transactions)))
-				tp.notifyWork()
-			}
-
-		case req := <-tp.getTxsCh:
-			// Copy transactions to avoid race condition when returned to caller
-			txCopy := make([]types.Transaction, len(transactions))
-			copy(txCopy, transactions)
-
-			// Clear the internal state
-			transactions = make([]types.Transaction, 0)
-			transactionKeys = make(map[txPoolKey]bool)
-			txHashMap = make(map[common.Hash]types.Transaction)
-
-			atomic.StoreInt64(&tp.count, 0)
-			req.reply <- txCopy
-
-		case req := <-tp.countCh:
-			req.reply <- len(transactions)
-
-		case req := <-tp.evictCh:
-			if len(transactions) <= req.count {
-				// Evict all
-				evicted := len(transactions)
-				transactions = make([]types.Transaction, 0)
-				transactionKeys = make(map[txPoolKey]bool)
-				txHashMap = make(map[common.Hash]types.Transaction)
-				atomic.StoreInt64(&tp.count, 0)
-				req.reply <- evicted
-				continue
-			}
-
-			// Sort a slice of indices to find the lowest gas price transactions
-			type txInfo struct {
-				index int
-				gas   uint64
-			}
-			infos := make([]txInfo, len(transactions))
-			for i, tx := range transactions {
-				infos[i] = txInfo{index: i, gas: tx.MaxGasPrice()}
-			}
-
-			// Sort by GasPrice ascending
-			sort.Slice(infos, func(i, j int) bool {
-				return infos[i].gas < infos[j].gas
-			})
-
-			// Create a set of indices to remove
-			toRemove := make(map[int]bool)
-			for i := 0; i < req.count; i++ {
-				toRemove[infos[i].index] = true
-			}
-
-			newTxs := make([]types.Transaction, 0, len(transactions)-req.count)
-			for i, tx := range transactions {
-				if toRemove[i] {
-					// Remove from maps
-					key := txPoolKey{addr: tx.FromAddress(), nonce: tx.GetNonce()}
-					delete(transactionKeys, key)
-					h := tx.Hash()
-					if h != (common.Hash{}) {
-						delete(txHashMap, h)
-					}
-				} else {
-					newTxs = append(newTxs, tx)
-				}
-			}
-
-			transactions = newTxs
-			atomic.StoreInt64(&tp.count, int64(len(transactions)))
-			req.reply <- req.count
-
-		case req := <-tp.getTxByHashCh:
-			if req.hash == (common.Hash{}) {
-				logger.Warn("GetTransactionByHash called with a zero hash value.")
-				req.reply <- getTxResp{tx: nil, ok: false}
-				continue
-			}
-
-			tx, ok := txHashMap[req.hash]
-			req.reply <- getTxResp{tx: tx, ok: ok}
-		}
-	}
+func (tp *TransactionPool) getShardIndex(addr common.Address) uint8 {
+	return addr[0] // 0-255 based on the first byte of the address
 }
 
 func (tp *TransactionPool) CountTransactions() int {
 	return int(atomic.LoadInt64(&tp.count))
 }
 
-func (tp *TransactionPool) EvictLowestGasPrice(count int) int {
-	reply := make(chan int, 1)
-	tp.evictCh <- evictReq{count: count, reply: reply}
-	return <-reply
+func (tp *TransactionPool) EvictLowestGasPrice(countToEvict int) int {
+	if countToEvict <= 0 {
+		return 0
+	}
+
+	type txInfo struct {
+		shardIdx int
+		index    int
+		gas      uint64
+		hash     common.Hash
+	}
+
+	var allInfos []txInfo
+
+	for i := 0; i < NumShards; i++ {
+		shard := tp.shards[i]
+		shard.mu.RLock()
+		for idx, tx := range shard.transactions {
+			allInfos = append(allInfos, txInfo{
+				shardIdx: i,
+				index:    idx,
+				gas:      tx.MaxGasPrice(),
+				hash:     tx.Hash(),
+			})
+		}
+		shard.mu.RUnlock()
+	}
+
+	if len(allInfos) <= countToEvict {
+		// Evict all
+		var totalEvicted int
+		for i := 0; i < NumShards; i++ {
+			shard := tp.shards[i]
+			shard.mu.Lock()
+			totalEvicted += len(shard.transactions)
+			shard.transactions = make([]types.Transaction, 0)
+			shard.transactionKeys = make(map[txPoolKey]bool)
+			shard.txHashMap = make(map[common.Hash]types.Transaction)
+			shard.mu.Unlock()
+		}
+		atomic.StoreInt64(&tp.count, 0)
+		return totalEvicted
+	}
+
+	// Sort by GasPrice ascending
+	sort.Slice(allInfos, func(i, j int) bool {
+		return allInfos[i].gas < allInfos[j].gas
+	})
+
+	// Group removals by shard
+	toRemoveByShard := make(map[int]map[common.Hash]bool)
+	for i := 0; i < countToEvict; i++ {
+		info := allInfos[i]
+		if toRemoveByShard[info.shardIdx] == nil {
+			toRemoveByShard[info.shardIdx] = make(map[common.Hash]bool)
+		}
+		toRemoveByShard[info.shardIdx][info.hash] = true
+	}
+
+	var totalEvicted int
+	for i, hashesToRemove := range toRemoveByShard {
+		shard := tp.shards[i]
+		shard.mu.Lock()
+
+		newTxs := make([]types.Transaction, 0, len(shard.transactions)-len(hashesToRemove))
+		for _, tx := range shard.transactions {
+			h := tx.Hash()
+			if hashesToRemove[h] {
+				key := txPoolKey{addr: tx.FromAddress(), nonce: tx.GetNonce()}
+				delete(shard.transactionKeys, key)
+				delete(shard.txHashMap, h)
+				totalEvicted++
+			} else {
+				newTxs = append(newTxs, tx)
+			}
+		}
+		shard.transactions = newTxs
+		shard.mu.Unlock()
+	}
+
+	atomic.AddInt64(&tp.count, int64(-totalEvicted))
+	return totalEvicted
 }
 
 func (tp *TransactionPool) AddTransaction(tx types.Transaction) error {
-	reply := make(chan error, 1)
-	tp.addTxCh <- addTxReq{tx: tx, reply: reply}
-	return <-reply
+	shardIdx := tp.getShardIndex(tx.FromAddress())
+	shard := tp.shards[shardIdx]
+
+	shard.mu.Lock()
+	defer shard.mu.Unlock()
+
+	key := txPoolKey{addr: tx.FromAddress(), nonce: tx.GetNonce()}
+	if shard.transactionKeys[key] {
+		logger.Info("Transaction already exists in pool, skipping key addr=%s nonce=%d", key.addr.Hex(), key.nonce)
+		return fmt.Errorf("transaction already exists in pool, skipping")
+	}
+
+	// CROSS-CHAIN DEBUG logic (unchanged)
+	if tx.ToAddress() == common.HexToAddress("0x00000000000000000000000000000000B429C0B2") {
+		logger.Info("📥 [POOL-ARRIVE] Cross-chain TX entered mempool: hash=%s from=%s nonce=%d pool_size=%d",
+			tx.Hash().Hex()[:16], tx.FromAddress().Hex()[:10], tx.GetNonce(), atomic.LoadInt64(&tp.count)+1)
+	}
+
+	shard.transactions = append(shard.transactions, tx)
+	shard.transactionKeys[key] = true
+
+	h := tx.Hash()
+	if h != (common.Hash{}) {
+		shard.txHashMap[h] = tx
+	}
+
+	atomic.AddInt64(&tp.count, 1)
+	tp.notifyWork()
+	return nil
 }
 
 func (tp *TransactionPool) AddTransactions(txs []types.Transaction) {
-	tp.addTxsCh <- txs
+	// Group transactions by shard to minimize lock contention
+	txsByShard := make(map[uint8][]types.Transaction)
+	for _, tx := range txs {
+		idx := tp.getShardIndex(tx.FromAddress())
+		txsByShard[idx] = append(txsByShard[idx], tx)
+	}
+
+	addedAny := false
+	var totalAdded int64
+
+	for idx, shardTxs := range txsByShard {
+		shard := tp.shards[idx]
+		shard.mu.Lock()
+
+		for _, tx := range shardTxs {
+			key := txPoolKey{addr: tx.FromAddress(), nonce: tx.GetNonce()}
+			if !shard.transactionKeys[key] {
+				shard.transactions = append(shard.transactions, tx)
+				shard.transactionKeys[key] = true
+
+				h := tx.Hash()
+				if h != (common.Hash{}) {
+					shard.txHashMap[h] = tx
+				}
+				totalAdded++
+				addedAny = true
+			}
+		}
+		shard.mu.Unlock()
+	}
+
+	if addedAny {
+		atomic.AddInt64(&tp.count, totalAdded)
+		tp.notifyWork()
+	}
 }
 
 // TransactionsWithAggSign returns transactions and aggregate sign
 // and clear transactions
 func (tp *TransactionPool) TransactionsWithAggSign() ([]types.Transaction, []byte) {
-	reply := make(chan []types.Transaction, 1)
-	tp.getTxsCh <- getTxsReq{reply: reply}
-	txs := <-reply
+	var allTxs []types.Transaction
+
+	for i := 0; i < NumShards; i++ {
+		shard := tp.shards[i]
+		shard.mu.Lock()
+		if len(shard.transactions) > 0 {
+			allTxs = append(allTxs, shard.transactions...)
+			shard.transactions = make([]types.Transaction, 0)
+			shard.transactionKeys = make(map[txPoolKey]bool)
+			shard.txHashMap = make(map[common.Hash]types.Transaction)
+		}
+		shard.mu.Unlock()
+	}
+
+	atomic.StoreInt64(&tp.count, 0)
 
 	// Preserving original behavior: aggregate sign returns nil
-	return txs, nil
+	return allTxs, nil
 }
 
 func (tp *TransactionPool) GetTransactionByHash(hashToFind common.Hash) (types.Transaction, bool) {
-	reply := make(chan getTxResp, 1)
-	tp.getTxByHashCh <- getTxReq{hash: hashToFind, reply: reply}
-	resp := <-reply
-	return resp.tx, resp.ok
+	if hashToFind == (common.Hash{}) {
+		logger.Warn("GetTransactionByHash called with a zero hash value.")
+		return nil, false
+	}
+
+	for i := 0; i < NumShards; i++ {
+		shard := tp.shards[i]
+		shard.mu.RLock()
+		tx, ok := shard.txHashMap[hashToFind]
+		shard.mu.RUnlock()
+		if ok {
+			return tx, true
+		}
+	}
+
+	return nil, false
 }
