d7047e1e409a650b385d132c18250d1a9aa9d7db
Tên của bạn <email@example.com>
2026-07-04 10:51:30 +0000

    refactor: remove SKIP_MEMPOOL_SIG_VERIFY environment variable checks from transaction validation logic

--- execution/pkg/blockchain/tx_processor/validation.go
+++ execution/pkg/blockchain/tx_processor/validation.go
@@ -8,7 +8,6 @@ import (
 	"sync/atomic"
 	"time"
 
-	"os"
 
 	eth_common "github.com/ethereum/go-ethereum/common"
 	"github.com/meta-node-blockchain/meta-node/pkg/blockchain"
@@ -244,24 +243,22 @@ func VerifyTransaction(
 		} else {
 			if !isCrossChainBatchSubmit {
 				if !LoadVerifiedSignature(txHash) {
-					if os.Getenv("SKIP_MEMPOOL_SIG_VERIFY") != "true" {
-						request := transaction.NewVerifyTransactionRequest(
-							tx.Hash(),
-							common.PubkeyFromBytes(as.PublicKeyBls()),
-							tx.Sign(),
-						)
-						if !request.Valid() {
-							logger.Error("BLS Verification Failed!")
-							logger.Error("  txHashHex: %s", txHash.Hex())
-							logger.Error("  FromAddress: %s", tx.FromAddress().Hex())
-							logger.Error("  ToAddress: %s", tx.ToAddress().Hex())
-							logger.Error("  SenderPubKey: %x", as.PublicKeyBls())
-							logger.Error("  SenderSign: %x", tx.Sign().Bytes())
-							logger.Error("  Hash() of TX according to SubNode: %x", tx.Hash().Bytes())
-							if !tx.ValidEthSign() {
-								logger.Error("  ETH Verification also failed!")
-								return transaction.InvalidSign
-							}
+					request := transaction.NewVerifyTransactionRequest(
+						tx.Hash(),
+						common.PubkeyFromBytes(as.PublicKeyBls()),
+						tx.Sign(),
+					)
+					if !request.Valid() {
+						logger.Error("BLS Verification Failed!")
+						logger.Error("  txHashHex: %s", txHash.Hex())
+						logger.Error("  FromAddress: %s", tx.FromAddress().Hex())
+						logger.Error("  ToAddress: %s", tx.ToAddress().Hex())
+						logger.Error("  SenderPubKey: %x", as.PublicKeyBls())
+						logger.Error("  SenderSign: %x", tx.Sign().Bytes())
+						logger.Error("  Hash() of TX according to SubNode: %x", tx.Hash().Bytes())
+						if !tx.ValidEthSign() {
+							logger.Error("  ETH Verification also failed!")
+							return transaction.InvalidSign
 						}
 					}
 					// Only cache on successful validation
@@ -444,11 +441,6 @@ func PreVerifySignatures(txs []types.Transaction, chainState *blockchain.ChainSt
 	verifyFn := func(tx types.Transaction) {
 		txHash := tx.Hash()
 		if LoadVerifiedSignature(txHash) {
-			return
-		}
-
-		if os.Getenv("SKIP_MEMPOOL_SIG_VERIFY") == "true" {
-			StoreVerifiedSignature(txHash)
 			return
 		}
 
